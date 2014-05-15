5afc96a21058926c2a2bb6a1b9526fb977bef762
laroze_a <laroze_a@epitech.eu>
2014-05-15 17:13:14 +0200

    Triangle + GL3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <GL/glew.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/Config.hpp>
 #include <SFML/Audio.hpp>
@@ -8,42 +7,43 @@
 #include <SFML/Network.hpp>
 #include <SFML/OpenGL.hpp>
 #include <GL3/gl3.h>
-
-int                     main()
+      
+int main()
 {
+  bool running = true;
+  sf::Event event;
   sf::ContextSettings settings;
+
   settings.depthBits = 32;
   settings.majorVersion = 3;
   settings.minorVersion = 3;
-  sf::RenderWindow window(sf::VideoMode(900,900), "OpenGL", sf::Style::Close, settings); 
-  bool                  running = true;
-  float                 vertices[] = {-0.5, -0.5, 0.0, 0.5, 0.5, -0.5};
-  sf::Event             event;
-  
-  settings.majorVersion = 3;
-  settings.minorVersion = 3;
-  window.setVerticalSyncEnabled(true);
+     
+  sf::RenderWindow window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, settings);    
+
   while (running)
     {
       window.clear();
       while (window.pollEvent(event))
-        {
-          if (event.type == sf::Event::Closed ||
-              sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-            running = false;
-          if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-            {
+	{
+	  if (event.type == sf::Event::Closed ||
+	      sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+	    running = false;
+	  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+	    {
 	      std::cout << "Space pressed" << std::endl;
-            }
-        }
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, vertices);
-      glEnableVertexAttribArray(0);
-      glDrawArrays(GL_TRIANGLES, 0, 3);
-      glDisableVertexAttribArray(0);
-      window.pushGLStates();
-      window.popGLStates();
+	    }
+	}
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+      glPushMatrix();
+      glBegin(GL_TRIANGLES);                 // Drawing Using Triangles
+      glVertex3f( 0, 1, 0);                  // Top
+      glVertex3f(-1,-1, 0);                  // Bottom Left
+      glVertex3f( 1,-1, 0);                  // Bottom Right
+      glEnd();
+      glPopMatrix();
+      glFlush();
       window.display();
     }
-  return 0;
+  return 0;     
 }
+
